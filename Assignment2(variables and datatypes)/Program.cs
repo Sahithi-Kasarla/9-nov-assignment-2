1d3aa41eee65b3cbf48fed447423e1844ae94f8c
Sahithi-kasarla <sahithikasarla123@gmail.com>
2023-11-09 15:56:02 +0530

    assignment 2

---
+++ Assignment2(variables and datatypes)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_variables_and_datatypes_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "sahithi";
+            int age = 22;
+            double height=5.5;
+            Console.WriteLine("my name is "+name);
+            Console.WriteLine("Iam " + age);
+            Console.WriteLine("my height is " + height);
+            Console.ReadKey();
+
+        }
+    }
+}
